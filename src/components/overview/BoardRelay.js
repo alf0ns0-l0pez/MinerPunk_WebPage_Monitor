98b28ac408842af477154ddefe3e3e6c710ce345
unknown <alfonsoloph@gmail.com>
2023-09-29 23:44:50 -0600

    add expand row and change relay board content

--- src/components/overview/BoardRelay.js
+++ src/components/overview/BoardRelay.js
@@ -1,70 +1,102 @@
-import './OverView.css'
+import { useState } from 'react';
+import './OverView.css';
 export default function BoardRelay(props) {
     const { board } = props;
+    const [expandView, setExpandView] = useState(false);
+    const [clickNONC, setClickNONC] = useState(false);
+    const [clickOpenClose, setOpenClose] = useState(false);
+
     return (
-        <section className='board-relay'>
-            <div className='board-logo'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
-            <h2 className='title-board'>{`${board.BOARDTYPE}  #${board.ID} | ${board.DESCRIPTION}`}</h2>
-            <div className='drop-form'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
-            <div className='relay-list'>
-                <h1 style={{ borderColor: 'red' }}>1</h1>
-                <h1 style={{ borderColor: 'red' }}>2</h1>
-                <h1 style={{ borderColor: 'green' }}>3</h1>
-                <h1 style={{ borderColor: 'green' }}>4</h1>
-                <h1 style={{ borderColor: 'red' }}>5</h1>
-                <h1 style={{ borderColor: 'red' }}>6</h1>
-                <h1 style={{ borderColor: 'green' }}>7</h1>
-                <h1 style={{ borderColor: 'red' }}>8</h1>
-            </div>
-            <form>
-                <h3 style={{ gridColumn: '1/3' }}>NO/NC</h3>
-                <h3>Relay No</h3>
-                <h3 style={{ gridColumn: '4/6' }}>State</h3>
-                <h4 style={{
-                    gridColumn: '1',
-                    textAlign: 'right',
-                    lineHeight: '1.5',
-                    paddingRight: '3px',
-                    backgroundColor: 'red',
-                    borderTopLeftRadius: '6px',
-                    borderBottomLeftRadius: '6px'
-                }}>
-                    NO</h4>
-                <h4 style={{
-                    gridColumn: '2',
-                    paddingLeft: '3px',
-                    lineHeight: '1.5',
-                    backgroundColor: 'green',
-                    borderTopRightRadius: '6px',
-                    borderBottomRightRadius: '6px'
-                }}>
-                    NC</h4>
-                <input type='number' min="1" max="8" />
-                <h4 style={{
-                    gridColumn: '4',
-                    textAlign: 'right',
-                    lineHeight: '1.5',
-                    paddingRight: '3px',
-                    backgroundColor: 'red',
-                    borderTopLeftRadius: '6px',
-                    borderBottomLeftRadius: '6px'
-                }}>
-                    Open</h4>
-                <h4 style={{
-                    gridColumn: '5',
-                    paddingLeft: '3px',
-                    lineHeight: '1.5',
-                    backgroundColor: 'green',
-                    borderTopRightRadius: '6px',
-                    borderBottomRightRadius: '6px'
-                }}>
-                    Close</h4>
-            </form>
-            <div className='board-details'> 
-                <p>{`Ip Address : ${board.IPADDRESS}`}</p>
-                <p>{`Port : ${board.PORT}`}</p>
-                <p>{`Serial Name : ${board.SERIALNAME}`}</p>
-            </div>
-        </section>
+        <>{
+
+            !expandView ?
+                <section className='board-row'>
+                    <div className='board-logo'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
+                    <h2 className='title-board'>{`${board.BOARDTYPE}  #${board.ID} | ${board.DESCRIPTION}`}</h2>
+                    <div className='drop-form' onClick={() => setExpandView(true)}>
+                        <img src=".\images\relayNormal.svg" alt="Logo" height='40px' />
+                    </div>
+                </section> :
+                <section className='board-relay'>
+                    <div className='board-logo'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
+                    <h2 className='title-board'>{`${board.BOARDTYPE}  #${board.ID} | ${board.DESCRIPTION}`}</h2>
+                    <div className='drop-form' onClick={() => setExpandView(false)}>
+                        <img src=".\images\relayNormal.svg" alt="Logo" height='40px' />
+                    </div>
+                    <h3 style={{ gridColumn: '2/4' }}>OPTO INPUTS:</h3>
+                    <div className='opto-list'>
+                        <h1 style={{ borderColor: 'red' }}>1</h1>
+                        <h1 style={{ borderColor: 'red' }}>2</h1>
+                        <h1 style={{ borderColor: 'green' }}>3</h1>
+                        <h1 style={{ borderColor: 'green' }}>4</h1>
+                    </div>
+                    <h3 style={{ gridColumn: '2/4' }}>RELAY OUTPUTS:</h3>
+                    <div className='relay-list'>
+                        <h1 style={{ borderColor: 'red' }}>1</h1>
+                        <h1 style={{ borderColor: 'red' }}>2</h1>
+                        <h1 style={{ borderColor: 'green' }}>3</h1>
+                        <h1 style={{ borderColor: 'green' }}>4</h1>
+                        <h1 style={{ borderColor: 'red' }}>5</h1>
+                        <h1 style={{ borderColor: 'red' }}>6</h1>
+                        <h1 style={{ borderColor: 'green' }}>7</h1>
+                        <h1 style={{ borderColor: 'red' }}>8</h1>
+                    </div>
+                    <form>
+                        <h3 style={{ gridColumn: '1/3' }}>NO/NC</h3>
+                        <h3>Relay No</h3>
+                        <h3 style={{ gridColumn: '4/6' }}>State</h3>
+                        <h4 style={{
+                            gridColumn: '1',
+                            textAlign: 'right',
+                            lineHeight: '1.5',
+                            paddingRight: '3px',
+                            backgroundColor: clickNONC ? 'grey' : 'green',
+                            borderTopLeftRadius: '6px',
+                            borderBottomLeftRadius: '6px'
+                        }}
+                            onClick={() => setClickNONC(false)}>
+                            NO</h4>
+                        <h4 style={{
+                            gridColumn: '2',
+                            paddingLeft: '3px',
+                            lineHeight: '1.5',
+                            backgroundColor: !clickNONC ? 'grey' : 'green',
+                            borderTopRightRadius: '6px',
+                            borderBottomRightRadius: '6px'
+                        }}
+                            onClick={() => setClickNONC(true)}>
+                            NC</h4>
+                        <input value={1} type='number' min="1" max="8" />
+                        <h4 style={{
+                            gridColumn: '4',
+                            textAlign: 'right',
+                            lineHeight: '1.5',
+                            paddingRight: '3px',
+                            backgroundColor: clickOpenClose ? 'grey' : 'green',
+                            borderTopLeftRadius: '6px',
+                            borderBottomLeftRadius: '6px'
+                        }}
+                            onClick={() => setOpenClose(false)}>
+                            Open</h4>
+                        <h4 style={{
+                            gridColumn: '5',
+                            paddingLeft: '3px',
+                            lineHeight: '1.5',
+                            backgroundColor: !clickOpenClose ? 'grey' : 'green',
+                            borderTopRightRadius: '6px',
+                            borderBottomRightRadius: '6px'
+                        }}
+                            onClick={() => setOpenClose(true)}>
+                            Close</h4>
+                    </form>
+                    <h3 style={{ gridColumn: '2/4' }}>CONNECTION DETAILS:</h3>
+                    <div className='board-details'>
+                        <p>Ip Address :<spam style={{ color: 'yellow' }}>{board.IPADDRESS}</spam></p>
+                        <p>Port :<spam style={{ color: 'yellow' }}>{board.PORT}</spam></p>
+                        <p>Serial Name :<spam style={{ color: 'yellow' }}>{board.SERIALNAME}</spam></p>
+                    </div>
+                </section>
+        }
+        </>
     );
 }
--- src/components/overview/BoardStepperMotor.js
+++ src/components/overview/BoardStepperMotor.js
@@ -1,10 +1,85 @@
+import { useState } from 'react';
 import './OverView.css'
 export default function BoardStepperMotor(props) {
     const { board } = props;
+    const [expandView, setExpandView] = useState(false);
+    const [clickNONC, setClickNONC] = useState(false);
+    const [clickOpenClose, setOpenClose] = useState(false);
     return (
-        <section className='board-motor'>
-            <div className='board-logo'><img src=".\images\motorNormal.svg" alt="Logo" height='40px' /></div>
-            <div className='drop-form'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
-        </section>
+        <>{
+
+            !expandView ?
+                <section className='board-row'>
+                    <div className='board-logo'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
+                    <h2 className='title-board'>{`${board.BOARDTYPE}  #${board.ID} | ${board.DESCRIPTION}`}</h2>
+                    <div className='drop-form' onClick={() => setExpandView(true)}><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
+                </section>
+                :
+                <section className='board-motor'>
+                    <div className='board-logo'><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
+                    <h2 className='title-board'>{`${board.BOARDTYPE}  #${board.ID} | ${board.DESCRIPTION}`}</h2>
+                    <div className='drop-form' onClick={() => setExpandView(false)}><img src=".\images\relayNormal.svg" alt="Logo" height='40px' /></div>
+                    <h3 style={{ gridColumn: '2/4' }}>OPTO INPUTS</h3>
+                    <div className='sensor-list'>
+                        <h1 style={{ borderColor: 'red' }}>1</h1>
+                        <h1 style={{ borderColor: 'red' }}>2</h1>
+                    </div>
+
+                    <form>
+                        <h3 style={{ gridColumn: '1/3' }}>Direction</h3>
+                        <h3>Steps No</h3>
+                        <h3 style={{ gridColumn: '4/6' }}>State</h3>
+                        <h4 style={{
+                            gridColumn: '1',
+                            textAlign: 'right',
+                            lineHeight: '1.5',
+                            paddingRight: '3px',
+                            backgroundColor: clickNONC ? 'grey' : 'green',
+                            borderTopLeftRadius: '6px',
+                            borderBottomLeftRadius: '6px'
+                        }}
+                            onClick={() => setClickNONC(false)}>
+                            LEFT</h4>
+                        <h4 style={{
+                            gridColumn: '2',
+                            paddingLeft: '3px',
+                            lineHeight: '1.5',
+                            backgroundColor: !clickNONC ? 'grey' : 'green',
+                            borderTopRightRadius: '6px',
+                            borderBottomRightRadius: '6px'
+                        }}
+                            onClick={() => setClickNONC(true)}>
+                            RIGHT</h4>
+                        <input value={1} type='number' min="1" />
+                        <h4 style={{
+                            gridColumn: '4',
+                            textAlign: 'right',
+                            lineHeight: '1.5',
+                            paddingRight: '3px',
+                            backgroundColor: clickOpenClose ? 'grey' : 'green',
+                            borderTopLeftRadius: '6px',
+                            borderBottomLeftRadius: '6px'
+                        }}
+                            onClick={() => setOpenClose(false)}>
+                            STOP</h4>
+                        <h4 style={{
+                            gridColumn: '5',
+                            paddingLeft: '3px',
+                            lineHeight: '1.5',
+                            backgroundColor: !clickOpenClose ? 'grey' : 'green',
+                            borderTopRightRadius: '6px',
+                            borderBottomRightRadius: '6px'
+                        }}
+                            onClick={() => setOpenClose(true)}>
+                            START</h4>
+                    </form>
+                    <div className='board-details'>
+                        <p>{`Ip Address : ${board.IPADDRESS}`}</p>
+                        <p>{`Port : ${board.PORT}`}</p>
+                        <p>{`Serial Name : ${board.SERIALNAME}`}</p>
+                    </div>
+                </section>
+        }
+        </>
     );
 }
